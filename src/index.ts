aab2fac47ae5db661c725184dc052dcafc353033
Yeechan Lu <wz.bluesnow@gmail.com>
2018-03-23 21:05:06 +0800

    Thoughts on train

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import * as t from 'io-ts';
-import { isString } from 'lodash';
+import { isFunction, isString } from 'lodash';
 import * as rt from './RuntimeType';
 
 export namespace Loi {
   export type Type<RT extends t.Any> = RT['_A'];
 
-  interface ILoiNumberOption {
+  interface INumberOption {
     name: string,
     max?: number
     min?: number
@@ -15,39 +15,81 @@ export namespace Loi {
     finite?: boolean
   }
 
-  export class LoiNumberType extends t.NumberType {
-    private loiOption: ILoiNumberOption[]
+  export class NumberFactory<T extends t.Any> extends t.Type<T['_A'], T['_O'], T['_I']> {
+    static loiTag = "number"
+    constructor() {
+      super(undefined, undefined, undefined, undefined);
+      throw new Error('This class cannot be constructored.');
+    }
+    private loiOption: INumberOption[]
 
     public max(limit: number) {
-      return asLoiNumber(t.refinement(this, (n) => n <= limit), this.loiOption, { name: `<=${limit}`, max: limit })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+        t.refinement(this, (n) => n <= limit),
+        this.loiOption,
+        { name: `<=${limit}`, max: limit }
+      )
     }
 
     public min(limit: number) {
-      return asLoiNumber(t.refinement(this, (n) => n >= limit), this.loiOption, { name: `>=${limit}`, min: limit })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+        t.refinement(this, (n) => n >= limit),
+        this.loiOption,
+        { name: `>=${limit}`, min: limit }
+      )
     }
 
     public greater(limit: number) {
-      return asLoiNumber(t.refinement(this, (n) => n > limit), this.loiOption, { name: `>${limit}`, greater: limit })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+         t.refinement(this, (n) => n > limit), this.loiOption, { name: `>${limit}`, greater: limit })
     }
 
     public less(limit: number) {
-      return asLoiNumber(t.refinement(this, (n) => n < limit), this.loiOption, { name: `<${limit}`, less: limit })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+        t.refinement(this, (n) => n < limit),
+        this.loiOption,
+        { name: `<${limit}`, less: limit }
+      )
     }
 
     public negative() {
-      return asLoiNumber(t.refinement(this, (n) => n < 0), this.loiOption, { name: `-`, less: 0 })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+        t.refinement(this, (n) => n < 0),
+        this.loiOption,
+        { name: `-`, less: 0 }
+      )
     }
 
     public positive() {
-      return asLoiNumber(t.refinement(this, (n) => n > 0), this.loiOption, { name: `+`, greater: 0 })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+        t.refinement(this, (n) => n > 0),
+        this.loiOption,
+        { name: `+`, greater: 0 }
+      )
     }
 
     public integer() {
-      return asLoiNumber(t.refinement(this, (n) => n % 1 === 0), this.loiOption, { name: `integer`, integer: true })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+        t.refinement(this, (n) => n % 1 === 0),
+        this.loiOption,
+        { name: `integer`, integer: true }
+      )
     }
 
     public finite() {
-      return asLoiNumber(t.refinement(this, (n) => Number.isFinite(n)), this.loiOption, { name: `finite`, finite: true })
+      return withFactory<NumberFactory<t.NumberType>, INumberOption>(
+        NumberFactory,
+        t.refinement(this, (n) => Number.isFinite(n)),
+        this.loiOption,
+        { name: `finite`, finite: true }
+      )
     }
 
     public default(value: this['_A']) {
@@ -55,21 +97,38 @@ export namespace Loi {
     }
   }
 
-  function asLoiNumber<T extends t.Any>(t: T, options: ILoiNumberOption[] = [], newOption?: ILoiNumberOption): LoiNumberType {
-    Object.setPrototypeOf(t, LoiNumberType.prototype);
-    const anyT: any = t;
+  function copyFactoryMethod(klass: Function, destination: any) {
+    Object.getOwnPropertyNames(klass.prototype).forEach((i) => {
+      if (i === 'constructor') return;
+      if (!isFunction(klass.prototype[i])) return;
+      destination[i] = klass.prototype[i].bind(destination);
+    })
+  }
+
+  function withFactory<
+    F extends t.Any,
+    LO extends { name?: string } = { name?: string },
+    T extends t.Type<F['_A'], F['_O'], F['_I']> = t.Type<F['_A'], F['_O'], F['_I']>
+  >(
+    factory: { new(...args: any[]): F, loiTag: string },
+    t: T,
+    options: LO[] = [],
+    newOption?: LO,
+  ): T & F {
+    const result: any = t;
     const newOptions = [...(options || []), ...(newOption ? [newOption] : [])];
-    anyT.name = `number${newOptions.length ? `(${newOptions.map((i) => i.name || JSON.stringify(i)).join(", ")})` : ""}`;
-    anyT.loiOption = newOptions;
-    return anyT;
+    result.name = `${factory.loiTag}${newOptions.length ? `(${newOptions.map((i) => i.name || JSON.stringify(i)).join(", ")})` : ""}`;
+    result.loiOption = newOptions;
+    copyFactoryMethod(factory, result);
+    return result;
   }
 
   export function number() {
-    return asLoiNumber(new t.NumberType(), []);
+    return withFactory<NumberFactory<t.NumberType>, INumberOption>(NumberFactory, new t.NumberType(), []);
   }
 
   export function numberOrStringNumber() {
-    return asLoiNumber(rt.convert(t.number, (i: string) => parseFloat(i), (i) => isString(i)), [{ name: "string" }]);
+    return withFactory<NumberFactory<t.NumberType>, INumberOption>(NumberFactory, rt.convert(t.number, (i: string) => parseFloat(i), (i) => isString(i)), [{ name: "string" }]);
   }
 
   export const union = t.union
