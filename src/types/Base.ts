14d326d4d4aa457171630efd17798304c7f007c3
Yeechan Lu <wz.bluesnow@gmail.com>
2018-03-25 16:38:20 +0800

    Add Base#nullAsUndefined

--- src/types/Base.ts
+++ src/types/Base.ts
@@ -4,7 +4,8 @@ import * as rt from '../RuntimeType';
 
 export interface IBaseOption<T> extends ILoiOption {
   name: string,
-  default?: T
+  default?: T,
+  nullAsUndefined?: boolean
 }
 
 export class BaseFactory<T extends t.Any> extends Factory<T> {
@@ -17,6 +18,14 @@ export class BaseFactory<T extends t.Any> extends Factory<T> {
     return metadata(BaseFactory.decorate(type), {
       parent: this,
       option: <IBaseOption<T>>{ name: `with default`, default: value }
+    });
+  }
+
+  public nullAsUndefined() {
+    const type = rt.nullAsUndefined(this);
+    return metadata(BaseFactory.decorate(type), {
+      parent: this,
+      option: <IBaseOption<T>>{ name: `null as undefined`, nullAsUndefined: true }
     });
   }
 }
