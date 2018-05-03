31c5622dc96d16aeaac8959b164b210893557c6c
Yeechan Lu <wz.bluesnow@gmail.com>
2018-05-04 01:51:23 +0800

    Make mimic more robust

--- src/utilties/mimic.ts
+++ src/utilties/mimic.ts
@@ -1,13 +1,18 @@
 import * as t from 'io-ts';
 
+function getObjectAllPropertyKeys(o: any): string[] {
+  const prototype = Object.getPrototypeOf(o)
+  const ownPropertyNames = Object.getOwnPropertyNames(o)
+  return prototype ? ownPropertyNames.concat(getObjectAllPropertyKeys(prototype)) : ownPropertyNames;
+}
+
 /** @internal */
 export function mimic<T, J extends t.Any>(fn: T, baseType: J) {
-  (<any>fn).is = baseType.is;
-  (<any>fn).validate = baseType.validate;
-  (<any>fn).encode = baseType.encode;
-  (<any>fn).asDecoder = baseType.asDecoder;
-  (<any>fn).asEncoder = baseType.asEncoder;
-  (<any>fn).decode = baseType.decode;
-  (<any>fn)._tag = (<any>baseType)._tag;
+  const keys = getObjectAllPropertyKeys(fn);
+  for (const i in baseType) {
+    if (keys.indexOf(i) < 0) {
+      (<any>fn)[i] = baseType[i]
+    }
+  }
   return fn as typeof fn & typeof baseType;
 }
