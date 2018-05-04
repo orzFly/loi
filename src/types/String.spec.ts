3ff82447b00ab2e845d4c0a6980f8a9a86b88abd
Yeechan Lu <wz.bluesnow@gmail.com>
2018-05-04 16:38:32 +0800

    Add String#length, String#min, String#max, String#regexp, String#regex, String#pattern

--- src/types/String.spec.ts
+++ src/types/String.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import * as t from 'io-ts';
 import { shouldNotValidate, shouldValidate } from '../test-helper.spec';
 import { string } from './String';
 
@@ -47,6 +48,110 @@ describe('types:String', () => {
       shouldNotValidate(test.decode(NaN))
       shouldNotValidate(test.decode(null))
       shouldNotValidate(test.decode(undefined))
+    })
+
+    it('length() should work', () => {
+      const test = string().length(5)
+
+      expect(test.name).to.be.eql("string(exact 5 chars)")
+      expect(shouldValidate(test.decode("12345"))).to.be.eql("12345")
+      shouldNotValidate(test.decode(""))
+      shouldNotValidate(test.decode("123"))
+      shouldNotValidate(test.decode("123456"))
+
+      expect(shouldValidate(test.decode(new String("12345")))).to.be.eql("12345")
+      shouldNotValidate(test.decode(new String("")))
+      shouldNotValidate(test.decode(new String("123")))
+      shouldNotValidate(test.decode(new String("123456")))
+      shouldNotValidate(test.decode(undefined))
+    })
+
+    it('min() should work', () => {
+      const test = string().min(5)
+
+      expect(test.name).to.be.eql("string(>=5 chars)")
+      expect(shouldValidate(test.decode("12345"))).to.be.eql("12345")
+      shouldNotValidate(test.decode(""))
+      shouldNotValidate(test.decode("123"))
+      expect(shouldValidate(test.decode("123456"))).to.be.eql("123456")
+
+      expect(shouldValidate(test.decode(new String("12345")))).to.be.eql("12345")
+      shouldNotValidate(test.decode(new String("")))
+      shouldNotValidate(test.decode(new String("123")))
+      expect(shouldValidate(test.decode(new String("123456")))).to.be.eql("123456")
+
+      shouldNotValidate(test.decode(undefined))
+    })
+
+    it('max() should work', () => {
+      const test = string().max(5)
+
+      expect(test.name).to.be.eql("string(<=5 chars)")
+      expect(shouldValidate(test.decode("12345"))).to.be.eql("12345")
+      expect(shouldValidate(test.decode(""))).to.be.eql("")
+      expect(shouldValidate(test.decode("123"))).to.be.eql("123")
+      shouldNotValidate(test.decode("123456"))
+
+      expect(shouldValidate(test.decode(new String("12345")))).to.be.eql("12345")
+      expect(shouldValidate(test.decode(new String("")))).to.be.eql("")
+      expect(shouldValidate(test.decode(new String("123")))).to.be.eql("123")
+      shouldNotValidate(test.decode(new String("123456")))
+      shouldNotValidate(test.decode(undefined))
+    })
+
+    const regexpTestCases = (test: t.Type<string, string, t.mixed>) => {
+      expect(shouldValidate(test.decode("12345"))).to.be.eql("12345")
+      expect(shouldValidate(test.decode(""))).to.be.eql("")
+      expect(shouldValidate(test.decode("123"))).to.be.eql("123")
+      shouldNotValidate(test.decode("abc456"))
+
+      expect(shouldValidate(test.decode(new String("12345")))).to.be.eql("12345")
+      expect(shouldValidate(test.decode(new String("")))).to.be.eql("")
+      expect(shouldValidate(test.decode(new String("123")))).to.be.eql("123")
+      shouldNotValidate(test.decode(new String("abc456")))
+      shouldNotValidate(test.decode(undefined))
+    }
+
+    it('regex(name) should work', () => {
+      const test = string().regex(/^\d*$/, "digitals only")
+
+      expect(test.name).to.be.eql("string(digitals only)")
+      regexpTestCases(test)
+    })
+
+    it('regex() should work', () => {
+      const test = string().regex(/^\d*$/)
+
+      expect(test.name).to.be.eql("string(regexp /^\\d*$/)")
+      regexpTestCases(test)
+    })
+
+    it('pattern(name) should work', () => {
+      const test = string().pattern(/^\d*$/, "digitals only")
+
+      expect(test.name).to.be.eql("string(digitals only)")
+      regexpTestCases(test)
+    })
+
+    it('pattern() should work', () => {
+      const test = string().pattern(/^\d*$/)
+
+      expect(test.name).to.be.eql("string(regexp /^\\d*$/)")
+      regexpTestCases(test)
+    })
+
+    it('regexp(name) should work', () => {
+      const test = string().regexp(/^\d*$/, "digitals only")
+
+      expect(test.name).to.be.eql("string(digitals only)")
+      regexpTestCases(test)
+    })
+
+    it('regexp() should work', () => {
+      const test = string().regexp(/^\d*$/)
+
+      expect(test.name).to.be.eql("string(regexp /^\\d*$/)")
+      regexpTestCases(test)
     })
   })
 })
--- src/types/String.ts
+++ src/types/String.ts
@@ -6,6 +6,10 @@ import { BaseFactory } from './Base';
 
 export interface IStringOption extends ILoiOption {
   name: string
+  length?: number
+  min?: number
+  max?: number
+  regex?: RegExp
 }
 
 export class StringType extends t.Type<string> {
@@ -26,6 +30,73 @@ export type StringFactoryType<T extends t.Any> = T & StringFactory<T> & BaseFact
 export class StringFactory<T extends t.Any> extends Factory<T> {
   static decorate<T extends t.Any>(t: T): StringFactoryType<T> {
     return BaseFactory.decorate(decorate<T, StringFactory<t.Type<T['_A'], T['_O'], T['_I']>>>(this, t));
+  }
+
+  /**
+   * Specifies the exact string length required
+   * @param limit the required string length
+   */
+  public length(limit: number) {
+    const type = t.refinement(this, (i) => i.length == limit)
+    return metadata(StringFactory.decorate<Clean<typeof type>>(type), {
+      parent: this,
+      option: <IStringOption>{ name: `exact ${limit} chars`, length: limit }
+    });
+  }
+
+  /**
+   * Specifies the maximum number of string characters
+   * @param limit the maximum number of string characters allowed
+   */
+  public max(limit: number) {
+    const type = t.refinement(this, (i) => i.length <= limit)
+    return metadata(StringFactory.decorate<Clean<typeof type>>(type), {
+      parent: this,
+      option: <IStringOption>{ name: `<=${limit} chars`, max: limit }
+    });
+  }
+
+  /**
+   * Specifies the minimum number string characters
+   * @param limit the minimum number of string characters required
+   */
+  public min(limit: number) {
+    const type = t.refinement(this, (i) => i.length >= limit)
+    return metadata(StringFactory.decorate<Clean<typeof type>>(type), {
+      parent: this,
+      option: <IStringOption>{ name: `>=${limit} chars`, min: limit }
+    });
+  }
+
+  /**
+   * Defines a regular expression rule
+   * @param pattern a regular expression object the string value must match against.
+   * @param name optional name for patterns (useful with multiple patterns)
+   */
+  public regex(pattern: RegExp, name: string = `regexp ${pattern.toString()}`) {
+    const type = t.refinement(this, (i) => i.match(pattern) !== null)
+    return metadata(StringFactory.decorate<Clean<typeof type>>(type), {
+      parent: this,
+      option: <IStringOption>{ name: name, regexp: pattern }
+    });
+  }
+
+  /**
+   * Defines a regular expression rule
+   * @param pattern a regular expression object the string value must match against.
+   * @param name optional name for patterns (useful with multiple patterns)
+   */
+  public regexp(pattern: RegExp, name?: string) {
+    return this.regex(pattern, name);
+  }
+
+  /**
+   * Defines a regular expression rule
+   * @param pattern a regular expression object the string value must match against.
+   * @param name optional name for patterns (useful with multiple patterns)
+   */
+  public pattern(pattern: RegExp, name?: string) {
+    return this.regex(pattern, name);
   }
 }
 
