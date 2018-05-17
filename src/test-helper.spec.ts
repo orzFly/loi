e86f4565392c5fed2dcef14fc16b7823187758c0
Yeechan Lu <wz.bluesnow@gmail.com>
2018-05-18 03:17:25 +0800

    Throw error in test-helper

--- src/test-helper.spec.ts
+++ src/test-helper.spec.ts
@@ -1,8 +1,13 @@
 import { Either } from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
+import { PathReporter } from 'io-ts/lib/PathReporter';
 
 export function shouldValidate<Right>(e: Either<t.ValidationError[], Right>) {
-  return e.fold((err) => { throw err }, (right) => right);
+  return e.fold((err) => {
+    const error = new Error(PathReporter.report(e).join("\n"));
+    (<any>error).details = err;
+    throw error;
+  }, (right) => right);
 }
 
 export function shouldNotValidate<Right>(e: Either<t.ValidationError[], Right>) {
