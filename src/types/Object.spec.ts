cf6ded8d8619e971eff99fb38c1b642704f996a5
Yeechan Lu <wz.bluesnow@gmail.com>
2018-05-31 20:39:06 +0800

    Merge object utilties and type

--- src/types/Object.spec.ts
+++ src/types/Object.spec.ts
@@ -1,13 +1,39 @@
 import { expect } from 'chai';
 import * as t from 'io-ts';
 import { shouldNotValidate, shouldValidate } from '../test-helper.spec';
-import { object } from './Object';
+import { nullablePartial, object } from './Object';
 
 // tslint:disable:no-unused-expression // chai to be NaN
 
 class MyRegExp extends RegExp { }
 
 describe('types:Object', () => {
+  describe('nullablePartial', () => {
+    it('should work', () => {
+      const test = nullablePartial({
+        key: t.boolean
+      })
+
+      expect(shouldValidate(test.decode({ key: true })).key).to.be.equal(true)
+      expect(shouldValidate(test.decode({ key: false })).key).to.be.equal(false)
+      expect(shouldValidate(test.decode({ key: null })).key).to.be.equal(undefined)
+      expect(shouldValidate(test.decode({ key: undefined })).key).to.be.equal(undefined)
+      expect(shouldValidate(test.decode({})).key).to.be.equal(undefined)
+    })
+
+    it('control group should not work', () => {
+      const test = t.partial({
+        key: t.boolean
+      })
+
+      expect(shouldValidate(test.decode({ key: true })).key).to.be.equal(true)
+      expect(shouldValidate(test.decode({ key: false })).key).to.be.equal(false)
+      shouldNotValidate(test.decode({ key: null }))
+      expect(shouldValidate(test.decode({ key: undefined })).key).to.be.equal(undefined)
+      expect(shouldValidate(test.decode({})).key).to.be.equal(undefined)
+    })
+  })
+
   describe('object', () => {
     it('should work', () => {
       const test = object({
--- src/types/Object.ts
+++ src/types/Object.ts
@@ -1,7 +1,7 @@
 import * as t from 'io-ts';
+import { LoiDecoratorNullAsUndefined } from '../decorators/nullAsUndefined';
 import { decorate, ILoiOption, LoiFactory, metadata } from '../utilties/factory';
-import { isString } from '../utilties/lodash';
-import { getNameFromProps, interfaceWithOptionals, strictInterfaceWithOptionals, violetInterfaceWithOptionals } from '../utilties/object';
+import { isString, objectMapValues } from '../utilties/lodash';
 import { LoiFactoryBase } from './Base';
 
 /** @internal */
@@ -11,6 +11,102 @@ export interface ILoiOptionObject extends ILoiOption {
   instanceof?: Function,
   strict?: boolean,
   violet?: boolean,
+}
+
+
+/** @internal */
+export function getNameFromProps(required: t.Props = {}, optional: t.Props = {}): string {
+  const result = `{ ${[
+    ...Object.keys(required).map((k) => `${k}: ${required[k].name}`),
+    ...Object.keys(optional).map((k) => `${k}?: ${optional[k].name}`),
+  ].join(', ')} }`
+
+  return result === "{  }" ? "{}" : result;
+}
+
+/** @internal */
+export function nullablePartial<P extends t.Props>(props: P, name?: string): t.PartialType<P, t.TypeOfPartialProps<P>, t.OutputOfPartialProps<P>> {
+  return t.partial(objectMapValues(props, (i) => new LoiDecoratorNullAsUndefined(i) as typeof i) as P, name);
+}
+
+/** @internal */
+export function interfaceWithOptionals<R extends t.Props, O extends t.Props>(
+  required: R,
+  optional: O,
+  name?: string
+): t.IntersectionType<[t.InterfaceType<R, t.TypeOfProps<R>, t.OutputOfProps<R>>, t.PartialType<O, t.TypeOfPartialProps<O>, t.OutputOfPartialProps<O>>], t.TypeOfProps<R> & t.TypeOfPartialProps<O>, t.OutputOfProps<R> & t.OutputOfPartialProps<O>> {
+  return t.intersection([t.interface(required), nullablePartial(optional)], name)
+}
+
+/**
+ * Return a new type that validates successfully only
+ * when the instance (object) contains no unknow properties.
+ *
+ * https://github.com/gunzip/digital-citizenship-functions/blob/cd5c57629cb188dbda4b03037fbb399115fd6d29/lib/utils/types.ts#L92
+ * https://github.com/gcanti/io-ts/issues/106
+ *
+ * @required  required properties
+ * @optional   optional object properties
+ * @internal
+ */
+export function strictInterfaceWithOptionals<R extends t.Props, O extends t.Props>(
+  required: R,
+  optional: O,
+  name: string
+): t.Type<t.TypeOfProps<R> & t.TypeOfPartialProps<O>, t.OutputOfProps<R> & t.OutputOfPartialProps<O>, {}> {
+  const loose = t.intersection([t.interface(required), nullablePartial(optional)]);
+  const props = Object.assign({}, required, optional);
+  const newType = new t.Type(
+    name,
+    (v): v is t.TypeOfProps<R> & t.TypeOfPartialProps<O> =>
+      loose.is(v) &&
+      Object.getOwnPropertyNames(v).every((k) => props.hasOwnProperty(k)),
+    (s, c) =>
+      loose.validate(s, c).chain((o) => {
+        const errors: t.Errors = Object.getOwnPropertyNames(o)
+          .map(
+            (key) =>
+              !props.hasOwnProperty(key)
+                ? t.getValidationError(o[key], t.appendContext(c, key, t.never))
+                : undefined
+          )
+          .filter((e): e is t.ValidationError => e !== undefined);
+        return errors.length ? t.failures(errors) : t.success(o);
+      }),
+    loose.encode
+  );
+  (<any>newType)._tag = 'StrictInterfaceWithOptionalsType';
+  return newType as any;
+}
+
+/** @internal */
+export function violetInterfaceWithOptionals<R extends t.Props, O extends t.Props>(
+  required: R,
+  optional: O,
+  name: string
+): t.Type<t.TypeOfProps<R> & t.TypeOfPartialProps<O>, t.OutputOfProps<R> & t.OutputOfPartialProps<O>, {}> {
+  const loose = t.intersection([t.interface(required), nullablePartial(optional)]);
+  const props = Object.assign({}, required, optional);
+  const newType = new t.Type(
+    name,
+    (v): v is t.TypeOfProps<R> & t.TypeOfPartialProps<O> => loose.is(v),
+    (s, c) =>
+      loose.validate(s, c).chain((o) => {
+        const keys = Object.getOwnPropertyNames(o)
+        const newObject: t.OutputOfProps<R> & t.OutputOfPartialProps<O> = {} as any;
+        const len = keys.length
+        for (let i = 0; i < len; i++) {
+          const key = keys[i]
+          if (props.hasOwnProperty(key)) {
+            newObject[key] = o[key];
+          }
+        }
+        return t.success(newObject)
+      }),
+    loose.encode
+  );
+  (<any>newType)._tag = 'VioletInterfaceWithOptionalsType';
+  return newType as any;
 }
 
 /** @internal */
